485721913c5f2e5791130c57a856283ea1ed33d6
happyheeon <heeonhappy@gmail.com>
2026-01-01 09:18:24 +0900

    add home_screen

--- gitflutter/lib/screens/home_screen.dart
+++ gitflutter/lib/screens/home_screen.dart
@@ -1,20 +1,46 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
 class HomeScreen extends StatefulWidget {
-  DateTime dt = DateTime.now();
-  HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Timer timer;
+  DateTime dt = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    // Update time every second
+    timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      setState(() {
+        dt = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel(); // Prevent memory leaks
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Timer")),
-      body: Column(children: [Text('${widget.dt.hour}:${widget.dt.minute}')]),
+      appBar: AppBar(title: const Text("Flutter Timer")),
+      body: Center(
+        child: Text(
+          "${dt.hour.toString().padLeft(2, '0')}:"
+          "${dt.minute.toString().padLeft(2, '0')}:"
+          "${dt.second.toString().padLeft(2, '0')}",
+          style: const TextStyle(fontSize: 40),
+        ),
+      ),
     );
   }
 }
